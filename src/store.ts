4855ef7de0df834dc70b1d160fc6566977d21fcc
kuba <jakubkulich@gmail.com>
2018-12-12 14:49:43 +0100

    feat(firebase): added firebase

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
+import { firebaseAction, firebaseMutations } from 'vuexfire'
 
 Vue.use(Vuex)
+firebase.initializeApp({
+  apiKey: "AIzaSyAW5mWlWwBfZOGAFz_D7rWrDLKxgjFxONY",
+  authDomain: "nakolesach-sk.firebaseapp.com",
+  databaseURL: "https://nakolesach-sk.firebaseio.com",
+  projectId: "nakolesach-sk",
+  storageBucket: "",
+  messagingSenderId: "968808681042"
+})
+
+const db = firebase.firestore()
+db.settings({ timestampsInSnapshots: true })
 
 export default new Vuex.Store({
   state: {
-
+    rides: [], // Will be bound as an array
   },
   mutations: {
-
+    ...firebaseMutations
   },
   actions: {
-
+    bindRef: firebaseAction(({ bindFirebaseRef }, { name, ref }) => {
+      bindFirebaseRef(name, ref)
+    }),
+    init: firebaseAction(({ bindFirebaseRef }) => {
+      bindFirebaseRef('rides', db.collection('rides'))
+    }),
   }
 })
